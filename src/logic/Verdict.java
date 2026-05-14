1ac989ff485b06e75dba86254bc4a6aa2732e30d
ohjun <ohjun@localhost>
2026-05-14 10:58:30 -0400

    initial commit

---
+++ src/logic/Verdict.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+/**
+ *
+ * @author ohjun
+ */
+public class Verdict
+{
+	private String caseTitle;
+	private String studentVerdict;
+	private String reason;
+}
