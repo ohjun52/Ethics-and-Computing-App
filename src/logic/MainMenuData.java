2fb229552856887ded4c8c9a058725f41d00e947
ohjun <ohjun@localhost>
2026-05-15 09:26:31 -0400

    delete main file add MainMenuData file to edit scenarios data

---
+++ src/logic/MainMenuData.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+/**
+ *
+ * @author ohjun
+ */
+public class MainMenuData
+{
+	public static EthicsCase[] cases = new EthicsCase[]
+	{
+		new PrivacyCase("锄禾日当午", "汗滴禾下土", "谁知盘中餐，粒粒皆辛苦")
+	};
+}
